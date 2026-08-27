a56f6cc1a06393b0f224eea26d808a69f3e5bbca
Kaveri Sahu <kaverisahu376@gmail.com>
2026-08-28 01:19:24 +0530

    order error solve

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const uri = process.env.MONGO_URL;
 const app = express();
 
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:"http://localhost:3001",
   credentials:true
 }));
 app.use(bodyParser.json());
