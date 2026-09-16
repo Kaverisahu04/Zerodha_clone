5fa11234fd8a01776ada1b5be07ce565566712c9
Kaveri Sahu <kaverisahu376@gmail.com>
2026-09-16 15:05:31 +0530

    Update deployed backends URLs2

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -32,7 +32,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://https://zerodha-backend-14si.onrender.com/signup",
+        "https://zerodha-backend-14si.onrender.com/signup",
         {
           ...inputValue,
         },
